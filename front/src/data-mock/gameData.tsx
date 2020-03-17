bfb406d88dcc733ff520a2297ce3ea76885345d9
theoWlkr <theo.walcker@gmail.com>
2020-03-17 14:15:36 +0100

    Data Edition

--- front/src/data-mock/gameData.tsx
+++ front/src/data-mock/gameData.tsx
@@ -1,21 +1,18 @@
-// @ts-ignore
 import {GameModel} from "../models/gameModel";
 
-let data: GameModel[] = [];
-data.push(new GameModel(
+let gameData = new GameModel(
     "Philippe des Pallières et Hervé Marly",
     30,
     7.8    ,
     8,
     "Le loup garou de thiercelieu",
-     12,
-     4,
+    12,
+    4,
     "Lui-Même",
     "",
     "2012-03-19T07:22Z",
     "<div> <p> Les Loups-Garous de Thiercelieux est un jeu de société d'ambiance dans lequel chaque joueur incarne un villageois ou un loup-garou, et dont le but général est : 	</p> <ul><li>Pour les villageois (dont certains ont des pouvoirs ou des particularités) : démasquer et tuer tous les loups-garous ; </li><li>Pour les loups-garous : d'éliminer tous les villageois et ne pas se faire démasquer ;</li> </ul></div>",
     12,
-    "Lui-Même"));
+    "Lui-Même");
 
-
-export default data;
+export default gameData;
