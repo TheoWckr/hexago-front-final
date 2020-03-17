dc2c5a8c570f458864148d7355632a2795c5ba84
theoWlkr <theo.walcker@gmail.com>
2020-03-17 17:37:35 +0100

    GDM01 - Ajout de mock et de model pour les Gender

---
+++ front/src/data-mock/GenderMock.tsx
@@ -0,0 +1,11 @@
+import {GenderModel} from "../models/genderModel";
+import {GameModel} from "../models/gameModel";
+
+
+export let genderMock : GenderModel = {
+    label : "Horror"
+}
+
+export let genderMockList : GenderModel[] = [
+    new GenderModel("Funny" ) , new GenderModel( "Culture" ),new GenderModel(" Quizz ")
+]
--- front/src/models/propsDeclaration.tsx
+++ front/src/models/propsDeclaration.tsx
@@ -1,7 +1,12 @@
 import {GameModel} from "./gameModel";
+import {GenderModel} from "./genderModel";
 
 /** Fichier de déclaration des proprs utilisés dans les differents components */
 
 export type GameProps = {
     game: GameModel
+}
+
+export type GenderProps = {
+    genders: GenderModel[]
 }
