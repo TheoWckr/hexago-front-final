fd7ed65d5c877f121a1ff0301de53972423d7357
miranto <miranto.razafindrazaka@epitech.eu>
2020-03-24 10:19:32 +0100

    begin at 1

--- front/src/game/list/listPagination.tsx
+++ front/src/game/list/listPagination.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import {Redirect, Route} from 'react-router-dom';
 import Pagination from '@material-ui/lab/Pagination';
 import detail from '../../data-mock/gameDetailMockList';
 import ListCard from "./listCard";
+import GameList from "./gameList";
 
 const handleChange = (event: any, value: any) => {
-    console.log(value);
+    return (
+        <Route path="/gamelist">
+            <GameList />
+        </Route>
+    );
 };
 
 const listPagination = () => {
-    const numberPage = Math.floor(detail.length / 12);
+    const numberPage = Math.floor(detail.length / 12) < 1 ? 1 : Math.floor(detail.length / 12);
 
     return (
         <div>
