c83b882e546df8c9c1359c69288de5a61f2ec68d
pipchild <pipchild@free.fr>
2020-03-30 11:49:46 +0200

    Author is an array

--- back/routes/gameDetails.js
+++ back/routes/gameDetails.js
@@ -31,7 +31,7 @@ function logHandleError(err) {
  * @apiGroup gamedetails
  *
  * @apiParam {String} name Name of a game
- * @apiParam {String} author Name of the author of a game
+ * @apiParam {Array} author Name of the author of a game
  * @apiParam {String} editor Name of the editor of a game
  * @apiParam {String} distributor Name of the distributor of a game
  * @apiParam {Date} releaseDate Date of the release of a game
@@ -54,7 +54,7 @@ function logHandleError(err) {
             "genre": [],
             "_id": "5e7a26929852d524bcfdffb3",
             "name": "Terraforming Mars",
-            "author": "Jacob Fryxelius",
+            "author": ["Jacob Fryxelius"],
             "editor": "Intrafin",
             "distributor": "Intrafin",
             "releaseDate": "2016-04-01T00:00:00.000Z",
@@ -70,7 +70,7 @@ function logHandleError(err) {
             "genre": [],
             "_id": "5e7a26929852d524bcfdffb1",
             "name": "Bunny Kingdom",
-            "author": "Richard Garfield",
+            "author": ["Richard Garfield"],
             "editor": "Iello",
             "distributor": "Iello",
             "releaseDate": "2018-01-01T00:00:00.000Z",
@@ -164,7 +164,65 @@ router.get('/', (req, res, next) => {
 
 
 //post create a game
-
+/**
+ * @api {post} /gamedetails/ Create a game
+ * @apiName POST gamedetails
+ * @apiGroup gamedetails
+ *
+ * @apiParam {String} name Name of a game, REQUIRED
+ * @apiParam {Array} author Name of the author of a game
+ * @apiParam {String} editor Name of the editor of a game
+ * @apiParam {String} distributor Name of the distributor of a game
+ * @apiParam {Date} releaseDate Date of the release of a game
+ * @apiParam {String} popularity Popularity score of a game
+ * @apiParam {String} playerMin Minimum number of player required to start a game
+ * @apiParam {String} playerMax Maximum number of player required to start a game
+ * @apiParam {String} gameLengthMin Minimum time value in minutes that would take a game
+ * @apiParam {String} gameLengthMax Maximum time value in minutes that would take a game
+ * @apiParam {String} minAge Minimum age advised to play the game
+ * @apiParam {String} description Description of a game
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost:3100/gamedetails?limit=2&offset=1&whatToSortBy=popularity&sortValue=-1
+ *
+ * @apiSuccessExample {json} Success-Response:
+ * {
+    "content": [
+        {
+            "genre": [],
+            "_id": "5e7a26929852d524bcfdffb3",
+            "name": "Terraforming Mars",
+            "author": "Jacob Fryxelius",
+            "editor": "Intrafin",
+            "distributor": "Intrafin",
+            "releaseDate": "2016-04-01T00:00:00.000Z",
+            "popularity": 13,
+            "playerMin": 1,
+            "playerMax": 5,
+            "gameLengthMin": 60,
+            "gameLengthMax": 120,
+            "minAge": 12,
+            "description": "L'ère de la domestication de Mars a commencé. Dans Terraforming Mars, de puissantes corporations travaillent pour rendre la Planète Rouge habitable. La température, l'oxygène et les océans sont les trois axes de développement principaux. Mais pour triompher, il faudra aussi construire des infrastructures pour les générations futures."
+        },
+        {
+            "genre": [],
+            "_id": "5e7a26929852d524bcfdffb1",
+            "name": "Bunny Kingdom",
+            "author": "Richard Garfield",
+            "editor": "Iello",
+            "distributor": "Iello",
+            "releaseDate": "2018-01-01T00:00:00.000Z",
+            "popularity": 12,
+            "playerMin": 2,
+            "playerMax": 4,
+            "gameLengthMin": 30,
+            "gameLengthMax": 60,
+            "minAge": 14,
+            "description": "Incarnez des seigneurs lapins et partez à la conquête d’un nouveau monde pour le compte du roi des lapins. Installez vos fiefs aux endroits stratégiques, exploitez les Ressources, bâtissez des Cités et préparez vos Missions secrètes pour voler à vos adversaires la victoire à la dernière minute. Chaque choix peut vous apporter les lauriers de la victoire ou l’ ombre de la défaite. Bunny Kingdom est un jeu de draft(sélection de cartes) et de placement qui se joue en 4 manches. Lors de chaque manche, les joueurs vont étendre leurs fiefs, augmenter leur Puissance et leur Richesse en construisant des Cités et en exploitant de nouvelles Ressources.Choisissez vos actions de chaque tour parmi les cartes que vous avez en main, avant de passer celles que vous ne jouez pas à vos adversaires. Chaque Manche se termine par un décompte de points dépendant de la Puissance et de la Richesse de vos Fiefs.Des Missions secrètes commandées par le roi des lapins au cours de la partie viendront sans doute perturber ce qui vous semblait établi avant le décompte final. À la fin de la partie, le joueur qui aura le plus fait prospérer son territoire sera nommé gouverneur du Nouveau Monde par le roi des lapins. Contient le plateau de jeu en version XL."
+        }
+    ]
+}
+ */
 router.post('/create', (req, res, next) => {
     // check if game already exists
     GameDetails.findOne({ 'name':req.body.name}, function(error, gameExists) {
