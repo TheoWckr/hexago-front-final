1171aba3812c257a16b5166b17c08ca0353c951a
Theo W <theo@walcker.fr>
2020-06-15 10:23:59 +0200

    Add User and Role Model

---
+++ front/src/models/userModel.ts
@@ -0,0 +1,41 @@
+import {GenreModel} from "./genreModel";
+import {RoleEnum} from "./roleEnum";
+
+export class UserModel {
+    _id?: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    dateOfCreation: Date;
+    dateLastConnection: Date;
+    dateOfBirth: Date;
+    active: boolean = true;
+    role: RoleEnum;
+
+
+    constructor(
+        {
+            id = "",
+            username= "",
+            firstName= "",
+            lastName= "",
+            email= "",
+            dateOfCreation = new Date,
+            dateLastConnection = new Date,
+            dateOfBirth= new Date,
+            active = true,
+            role= RoleEnum.STANDARD
+        }) {
+        this._id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfCreation = dateOfCreation;
+        this.dateLastConnection = dateLastConnection;
+        this.dateOfBirth = dateOfBirth;
+        this.active = active;
+        this.role = role;
+    }
+}
