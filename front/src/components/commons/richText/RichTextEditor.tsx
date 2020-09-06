23f8a35f69cfcaa0d2ecfb10ba86e697d4dc6fd6
Theo W <theo@walcker.fr>
2020-09-06 11:17:44 +0200

    Change of Rich text editor, using tinyMCE

---
+++ front/src/components/commons/richText/RichTextEditor.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Editor} from "@tinymce/tinymce-react";
+
+export const RichTextEditor = (props: {
+                                   handleEditorChange : (content : string, editor :any) => void ,
+                                   initialValue? : string,
+                                    height? : string
+                               }) => {
+    console.log("initial value", props.initialValue);
+
+    return (
+        <Editor
+            initialValue={props.initialValue ? props.initialValue : "Description"}
+            init={{
+                height: props.height ? props.height : "250",
+                menubar: false,
+                toolbar:
+                    'undo redo | formatselect | bold italic backcolor | \
+                    alignleft aligncenter alignright alignjustify | \
+                    bullist numlist outdent indent | removeformat | help'
+            }}
+            onEditorChange={props.handleEditorChange}
+        />
+    )
+}
