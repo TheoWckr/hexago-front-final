722e125948c5613676f3d8701b178a70fbd8a5d9
theoWlkr <theo.walcker@gmail.com>
2020-09-28 12:04:09 +0200

    Adding eventServiceType

---
+++ front/src/models/service/eventServiceType.ts
@@ -0,0 +1,26 @@
+export interface createEventForm{
+    date : Date;
+    duration :number;
+    minPlayers: number;
+    maxPlayers: number;
+    listGames?: string[];
+    locationId : string;
+    phone: string;
+    details: string;
+}
+
+
+export interface getEvent{
+    _id: string;
+}
+
+export interface searchEvent{
+    date?: Date;
+    locationId?: String;
+    listGames?: String[];
+    showEventFull?: boolean;
+    limit: number;
+    offset:number;
+
+}
+
