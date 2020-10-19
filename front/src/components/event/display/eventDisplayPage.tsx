c800fe194f4a7d48440a583fa2746346bb55d323
miranto <miranto.razafindrazaka@epitech.eu>
2020-10-19 17:15:54 +0200

    page event

--- front/src/components/event/display/eventDisplayPage.tsx
+++ front/src/components/event/display/eventDisplayPage.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
+import {useParams} from "react-router";
+import {Button, Grid, Typography} from "@material-ui/core";
+import './EventDisplay.css';
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        btn: {
+            color: '#312783',
+            fontSize: '1.1rem',
+            borderWidth: 2,
+            borderStyle: 'solid',
+            borderRadius: 50,
+            borderColor: '#312783',
+        },
+        btnDetail: {
+            color: '#312783',
+            fontSize: '0.7rem',
+            borderWidth: 2,
+            borderStyle: 'solid',
+            borderColor: '#312783',
+        }
+    }),
+);
 
 const EventDisplayPage = () => {
+    let {id} = useParams();
+    const classe = useStyles();
+
     return (
-      <div>
-          Event display page
-      </div>
+        <Grid container className="main">
+            <Grid item xs={12} className="box">
+                <Typography className="event-title">
+                    Nom de l'évènement
+                </Typography>
+            </Grid>
+            <Grid item xs={6} className="box">
+                <div className="box-detail">
+                    <Grid container>
+                        <Grid item xs={12} className="title-detail">
+                            <Typography>Le 25 novembre 2020</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography>A l'affiche:</Typography>
+                            <Button variant="outlined" className={classe.btnDetail}>Monopoly</Button>
+                            <Button variant="outlined" className={classe.btnDetail}>Time's Up</Button>
+                            <Button variant="outlined" className={classe.btnDetail}>Dominos</Button>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Grid>
+            <Grid item xs={6} className="box">
+                <div className="box-map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7449.505429822984!2d6.173500846207366!3d48.69022845072249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794986e17a692cd%3A0x4ed671b10d82498d!2sNancy!5e0!3m2!1sfr!2sfr!4v1600071064410!5m2!1sfr!2sfr"
+                        className="map"
+                        allowFullScreen={true}
+                        aria-hidden="false"/>
+                </div>
+            </Grid>
+            <Grid item xs={12} className="box-button">
+                <Button variant="outlined" className={classe.btn}>Participer à l'évènement</Button>
+            </Grid>
+        </Grid>
     );
 }
 
