8670ae79e2e866715e53370723d032a5422562d2
Theo W <theo@walcker.fr>
2020-11-30 10:07:59 +0100

    Reprise la recherche de jeux

--- front/src/components/game/list/gameList.tsx
+++ front/src/components/game/list/gameList.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import ListPagination from "./listPagination";
 import {GameSearchPanel} from "../search/GameSearchPanel";
+import GameSearchProps from "../../../models/game/gameSearch";
 
 const GameList = () => {
+    const initialState : GameSearchProps = {
+        genres: [],
+    }
+
+    const [gameSearch, setGameSearch] = useState<GameSearchProps>(initialState)
     return (
         <div>
-          <GameSearchPanel />
-        <ListPagination/>
+          <GameSearchPanel setSearch={setGameSearch}/>
+            <ListPagination search={gameSearch}/>
         </div>
     );
 };
