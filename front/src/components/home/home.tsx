baf8b542c8bb0b0fef0639b8d4756ebd4d2cdfb9
miranto <miranto.razafindrazaka@epitech.eu>
2020-12-11 13:09:25 +0100

    fix responsive

--- front/src/components/home/home.tsx
+++ front/src/components/home/home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Typography, Paper} from "@material-ui/core";
+import {Typography, Paper, Container} from "@material-ui/core";
 import {About} from "./about";
 import {makeStyles} from "@material-ui/core/styles";
 import {EventCity} from "./eventCity";
@@ -8,7 +8,7 @@ import './home.css';
 const useStyles = makeStyles({
     text: {
         textAlign: 'center',
-        fontSize: '1.3rem',
+        fontSize: '1.1rem',
         padding: '1rem 0'
     },
 });
@@ -19,20 +19,22 @@ export const Home = () => {
     return (
         <div className={'remainHeight'}>
             <About/>
-            <EventCity />
+            <EventCity/>
             <Paper elevation={3}>
-                <Typography className={classes.text}>
-                    Hexago est un site de mise en relation de joueurs amateurs et passionnés.
-                </Typography>
-                <Typography className={classes.text}>Choisissez un jeu, une
-                    ville et une date, et laissez vous guider par l'application.
-                </Typography>
-                <Typography className={classes.text}>Les évènements peuvent se produire dans
-                    des lieux publics (dans un bar) ou privés (chez un particulier).
-                </Typography>
-                <Typography className={classes.text}> Le service est exclusivement accessible
-                    aux personnes majeures.
-                </Typography>
+                <Container>
+                    <Typography className={classes.text}>
+                        Hexago est un site de mise en relation de joueurs amateurs et passionnés.
+                    </Typography>
+                    <Typography className={classes.text}>Choisissez un jeu, une
+                        ville et une date, et laissez vous guider par l'application.
+                    </Typography>
+                    <Typography className={classes.text}>Les évènements peuvent se produire dans
+                        des lieux publics (dans un bar) ou privés (chez un particulier).
+                    </Typography>
+                    <Typography className={classes.text}> Le service est exclusivement accessible
+                        aux personnes majeures.
+                    </Typography>
+                </Container>
             </Paper>
         </div>
     )
