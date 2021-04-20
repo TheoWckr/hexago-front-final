e49a60767df1c7b472d9e0ad4a5935148825ce14
Theo W <theo@walcker.fr>
2021-04-20 07:34:18 +0200

    fix main adress

--- src/utils/utilsAxios.ts
+++ src/utils/utilsAxios.ts
@@ -3,8 +3,8 @@ import {AxiosResponse} from "axios";
 export const axios = require('axios') ;
 
 
-//export const MAIN_ADRESS = "https://hexago-back.herokuapp.com";
-export const MAIN_ADRESS = "http://localhost:3100/";
+export const MAIN_ADRESS = "https://hexago-back.herokuapp.com/";
+//export const MAIN_ADRESS = "http://localhost:3100/";
 let contentType = 'Content-Type';
 export const UtilsAxios = {
         /**
